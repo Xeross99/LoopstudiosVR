2b4f9a95e41ecb11a8d88f52f1ae9680650d2078
Xeross99 <m.krzysteczko@icloud.com>
2023-03-15 19:57:06 +0100

    Hamburger menu bug fix

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const menuElement = document.getElementById("menu")
 const btnElement = document.getElementById("menuBtn")
+const btnElement2 = document.getElementById("menuBtn2")
 
 const toggleMenu = () => {
     menuElement.classList.toggle("showMenu")
 }
 
 btnElement.addEventListener("click", toggleMenu)
+btnElement2.addEventListener("click", toggleMenu)
